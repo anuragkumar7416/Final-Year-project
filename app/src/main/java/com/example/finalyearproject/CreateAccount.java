76f2b83e93ad7c711bdbdad42284c0be0b04b39d
Anurag Kumar <anuragkumar7416@gmail.com>
2022-01-13 21:08:08 +0530

    2nd presentation

--- app/src/main/java/com/example/finalyearproject/CreateAccount.java
+++ app/src/main/java/com/example/finalyearproject/CreateAccount.java
@@ -1,14 +1,68 @@
 package com.example.finalyearproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.finalyearproject.Models.mentees;
+import com.example.finalyearproject.databinding.ActivityCreateAccountBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAccount extends AppCompatActivity {
+
+     private FirebaseAuth auth;
+     FirebaseDatabase database;
+     ActivityCreateAccountBinding  binding;
+     ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(CreateAccount.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We're creating your account");
+
+        binding.btCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.show();
+                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etConfirmPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            progressDialog.dismiss();
+                            mentees mentee = new mentees(binding.etusername.getText().toString(),
+                                    binding.etPhoneNo.getText().toString(),
+                                    binding.etEmail.getText().toString(),
+                                    binding.etConfirmPassword.getText().toString());
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Mentees").child(id).setValue(mentee);
+
+                            Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_LONG).show();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(),"error",Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalyearproject/SignIn.java
+++ app/src/main/java/com/example/finalyearproject/SignIn.java
@@ -1,14 +1,65 @@
 package com.example.finalyearproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.tv.TvContract;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.finalyearproject.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
+
+
+    ActivitySignInBinding binding;
+    private FirebaseAuth auth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(SignIn.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Login into  your account");
+
+        binding.btSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.show();
+                auth.signInWithEmailAndPassword(binding.etEmail.getText().toString(),
+                        binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    progressDialog.dismiss();
+                                    Intent intent = new Intent(SignIn.this,MainActivity.class);
+                                    startActivity(intent);
+
+                                } else {
+
+                                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
     }
 }
