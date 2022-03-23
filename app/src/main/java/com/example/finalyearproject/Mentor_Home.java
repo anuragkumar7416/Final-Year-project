1ab299d7c5b04519bef91e037abdebee64853a78
Anurag Kumar <anuragkumar7416@gmail.com>
2022-03-23 22:55:10 +0530

    Wednesday-home

--- app/src/main/java/com/example/finalyearproject/Mentor_Home.java
+++ app/src/main/java/com/example/finalyearproject/Mentor_Home.java
@@ -1,13 +1,26 @@
 package com.example.finalyearproject;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+
+import com.example.finalyearproject.databinding.ActivityMentorHomeBinding;
+import com.example.finalyearproject.fragments.AccountFragment;
+import com.example.finalyearproject.fragments.CategoriesFragment;
+import com.example.finalyearproject.fragments.HomeFragment;
+import com.example.finalyearproject.fragments.MentorChatFragment;
+import com.example.finalyearproject.fragments.MentorHomeFragment;
+import com.example.finalyearproject.fragments.MentorProfileFragment;
+import com.example.finalyearproject.fragments.Mentorsfragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
@@ -19,6 +32,8 @@ public class Mentor_Home extends AppCompatActivity {
 
     EditText secretCodeBox;
     Button joinBtn, shareBtn;
+    BottomNavigationView bottomNavigationView;
+    ActivityMentorHomeBinding binding;
 //    private jit JitsiMeet;
 //    private BuildersKt JitsiMeetActivity;
 
@@ -26,33 +41,57 @@ public class Mentor_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mentor_home);
+        getSupportActionBar().hide();
 
-        secretCodeBox = findViewById(R.id.codeBox);
-        joinBtn = findViewById(R.id.joinBtn);
-        shareBtn = findViewById(R.id.shareBtn);
-
-        joinBtn.setOnClickListener(new View.OnClickListener(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.mentorlayoutNav,new MentorHomeFragment()).commit();
+        bottomNavigationView = findViewById(R.id.mentorbottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v){
-                if(secretCodeBox.getText().toString().isEmpty()){
-                    Toast.makeText(Mentor_Home.this,"Please Enter The Code",Toast.LENGTH_SHORT).show();
-                }else{
-                    try{
-                        JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                                .setServerURL(new URL("https:/meet.jit.si"))
-                                .setRoom(secretCodeBox.getText().toString())
-                                .setAudioOnly(true)
-                                .setWelcomePageEnabled(false)
-                                .build();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment temp = null;
+                switch (item.getItemId()){
+                    case R.id.mentor_home:
+                        temp = new MentorHomeFragment();
+                        break;
+                    case R.id.mentor_chats:
+                        temp = new MentorChatFragment();
+                        break;
+                    case R.id.mentor_profile:
+                        temp = new MentorProfileFragment();
+                        break;
 
-                        JitsiMeetActivity.launch(Mentor_Home.this,options);
-
-                    }catch (MalformedURLException e){
-                        e.printStackTrace();
-                    }
                 }
+                getSupportFragmentManager().beginTransaction().replace(R.id.mentorlayoutNav,temp).commit();
+                return true;
             }
         });
+
+//        secretCodeBox = findViewById(R.id.codeBox);
+//        joinBtn = findViewById(R.id.joinBtn);
+//        shareBtn = findViewById(R.id.shareBtn);
+//
+//        joinBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                if(secretCodeBox.getText().toString().isEmpty()){
+//                    Toast.makeText(Mentor_Home.this,"Please Enter The Code",Toast.LENGTH_SHORT).show();
+//                }else{
+//                    try{
+//                        JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+//                                .setServerURL(new URL("https:/meet.jit.si"))
+//                                .setRoom(secretCodeBox.getText().toString())
+//                                .setAudioOnly(true)
+//                                .setWelcomePageEnabled(false)
+//                                .build();
+//
+//                        JitsiMeetActivity.launch(Mentor_Home.this,options);
+//
+//                    }catch (MalformedURLException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        });
 
 
     }
